59c48d4d789ec160ba908ac8f7ff33d63cc071e5
KananAmir <amirovknn@gmail.com>
2024-11-29 17:59:42 +0400

    react intro, jsx, fragment, props

---
+++ 01.first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <label htmlFor="search">Search</label>
+      <input id="search" />
+    </div>
+  );
+}
+
+export default App;
